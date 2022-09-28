ae09abea4262346b58fb45de0f0845e996bd9633
Andrea Amantini <lo.zampino@gmail.com>
2022-09-28 14:23:11 +0200

    Add notebook

---
+++ notebooks/clerk_to_markdown.clj
@@ -0,0 +1,29 @@
+;; # 🗜 Clerk to Markdown
+(ns clerk_to_markdown
+  (:require [nextjournal.clerk :as clerk]
+            [nextjournal.clerk.parser :as clerk.parser]
+            [nextjournal.markdown.transform :as md.transform]))
+
+;; This notebook shows how to transform a Clojure namespace with Clerk style _literal fragments_ into a [markdown](https://daringfireball.net/projects/markdown/) file.
+
+(def this-notebook "notebooks/clerk_to_markdown.clj")
+
+;; A clerk notebook is composed of blocks, of type `:markdown` and `:code`. With recent additions to the markdown library we can turn our markdown AST data back into markdown text.
+
+;; This function turns a Clerk block into a string
+
+(defn block->md [{:as block :keys [type text doc]}]
+  (case type
+    :code (str "```clojure\n" text "\n```\n\n")
+    :markdown (md.transform/->md doc)))
+
+;; to put everything together, parse this notebook with Clerk and emit markdown as follows.
+
+^{::clerk/viewer '(fn [s _] (v/html [:pre s]))}
+(->> this-notebook
+     (clerk.parser/parse-file {:doc? true})
+     :blocks
+     (map block->md)
+     (apply str))
+
+;; To learn more about clerk visit our [github page](https://github.com/nextjournal/clerk).

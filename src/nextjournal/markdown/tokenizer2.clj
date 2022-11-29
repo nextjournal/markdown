30a4d4d4d2caf0cefc07abb93a383e3e16fb0d79
Michiel Borkent <michielborkent@gmail.com>
2022-11-29 12:45:39 +0100

    wip [skip ci]

--- src/nextjournal/markdown/tokenizer2.clj
+++ src/nextjournal/markdown/tokenizer2.clj
@@ -35,9 +35,117 @@
   (find-next-non-space {:line "    hello there" :offset 0 :column 0})
   )
 
-(defn incorporate-line [line] ;; https://github.com/commonmark/commonmark.js/blob/9a16ff4fb8111bc0f71e03206f5e3bdbf7c69e8d/lib/blocks.js#L738
-  (let [all-matched true
-        ])
+(defn get-last-child [{:keys [container]}]
+  ;; TODO
+  )
+
+(defn open? [node]
+  ;; TODO
+  )
+
+(defn container-type [container]
+  ;; TODO
+  )
+
+(def blocks {:document {:continue (fn [_] 0)
+                        :finalize (fn [])
+                        :can-contain? (fn [t] (= "item" t))
+                        :accepts-lines false}
+             :list     {:continue (fn [_] 0)
+                        :finalize (fn []
+                                    ;; TODO
+                                    )
+                        :can-contain? (fn [t] (= "item" t))
+                        :accepts-lines false}
+
+             :block-quote {:continue (fn [_]
+                                       ;; TODO
+                                       )
+                           :finalize (fn []
+                                       ;; TODO
+                                       )
+                           :can-contain? (fn [t] (= "item" t))
+                           :accepts-lines false}
+             :item {:continue (fn [_]
+                                ;; TODO
+                                )
+                    :finalize (fn []
+                                ;; TODO
+                                )
+                    :can-contain? (fn [t] (= "item" t))
+                    :accepts-lines false}
+             :heading {:continue (fn [_]
+                                   ;; TODO
+                                   )
+                       :finalize (fn []
+                                   ;; TODO
+                                   )
+                       :can-contain? (fn [t] (= "item" t))
+                       :accepts-lines false}
+             :thematic-break {:continue (fn [_]
+                                          ;; TODO
+                                          )
+                              :finalize (fn []
+                                          ;; TODO
+                                          )
+                              :can-contain? (fn [t] (= "item" t))
+                              :accepts-lines false}
+             :code-block {:continue (fn [_]
+                                      ;; TODO
+                                      )
+                          :finalize (fn []
+                                      ;; TODO
+                                      )
+                          :can-contain? (fn [t] (= "item" t))
+                          :accepts-lines true}
+             :html-block {:continue (fn [_]
+                                      ;; TODO
+                                      )
+                          :finalize (fn [_]
+                                      ;; TODO
+                                      )
+                          :can-contain? (fn [t] (= "item" t))
+                          :accepts-lines true}
+             :paragraph {:continue (fn [_]
+                                     ;; TODO
+                                     )
+                         :finalize (fn []
+                                     ;; TODO
+                                     )
+                         :can-contain? (fn [t] (= "item" t))
+                         :accepts-lines true}})
+
+(defn parent [container]
+  ;; TODO
+  )
+
+(defn incorporate-line [{:keys [doc tip line-number line] :as state}] ;; https://github.com/commonmark/commonmark.js/blob/9a16ff4fb8111bc0f71e03206f5e3bdbf7c69e8d/lib/blocks.js#L738
+  (let [container doc
+        all-matched true
+        old-tip tip
+        offset 0
+        column 0
+        blank false
+        partially-consumed-tab false
+        line-number (inc line-number)
+        ;; TODO: https://github.com/commonmark/commonmark.js/blob/9a16ff4fb8111bc0f71e03206f5e3bdbf7c69e8d/lib/blocks.js#L750
+
+        current-line line
+        last-child (get-last-child container)
+        [container] (loop [container last-child]
+                      (if (open? last-child)
+                        ;; TODO, what do to with the result of find-next-non-space?
+                        (let [next-non-space-state (find-next-non-space {:line line :offset offset :column column})
+                              state (merge state next-non-space-state)
+                              ct (container-type container)
+                              continue-fn (get blocks ct)]
+                          (case (continue-fn state)
+                            0 (recur (get-last-child container)) ;; recur
+                            1 [(parent container)] ;; recur
+                            2 (recur (get-last-child container))
+                            (throw (ex-info "continue returned illegal value, must be 0, 1 or 2" {}))
+                            ))))]
+    )
   )
 
 (defn tokenize [input]
@@ -59,7 +167,7 @@
 ;;;; Scratch
 
 (comment
-  
+
   (require '[nextjournal.markdown :as original])
   (tokenize "first paragraph
 

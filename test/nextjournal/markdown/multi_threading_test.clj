4f78044db05bacb0590ebb76208aec17800e671a
Andrea Amantini <lo.zampino@gmail.com>
2023-10-13 17:56:50 +0200

    Reproduce concurrent ctx-access issue in a test
    Restore GraalJS and Java versions to previous

---
+++ test/nextjournal/markdown/multi_threading_test.clj
@@ -0,0 +1,16 @@
+(ns nextjournal.markdown.multi-threading-test
+  (:require [clojure.test :as t :refer [deftest testing is]]
+            [nextjournal.markdown :as md]))
+
+(deftest multithreading
+  (let [!exs (atom [])
+        proc (fn []
+               (try (md/tokenize (slurp "notebooks/reference.md"))
+                    (catch IllegalStateException e
+                      (swap! !exs conj e))))
+        t1 (new Thread proc)
+        t2 (new Thread proc)]
+
+    (.start t1) (.start t2)
+    (.join t1) (.join t2)
+    (is (zero? (count @!exs)))))

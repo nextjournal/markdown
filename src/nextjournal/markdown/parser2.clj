f42219a4a973304f0dd238173ff650f4a928a5a3
Andrea Amantini <lo.zampino@gmail.com>
2024-04-03 14:25:44 +0200

    Todo lists

--- src/nextjournal/markdown/parser2.clj
+++ src/nextjournal/markdown/parser2.clj
@@ -2,6 +2,8 @@
   (:require [clojure.zip :as z]
             [nextjournal.markdown.parser :as parser])
   (:import (org.commonmark.parser Parser)
+           (org.commonmark.ext.task.list.items TaskListItemsExtension
+                                               TaskListItemMarker)
            (org.commonmark.node Node AbstractVisitor
             ;;;;;;;;;; node types ;;;;;;;;;;;;;;;;;;
                                 Document
@@ -18,19 +20,22 @@
 
                                 ListItem
                                 Link
+                                ThematicBreak
                                 SoftLineBreak
                                 HardLineBreak
                                 Image)))
 
 (set! *warn-on-reflection* true)
 
-(def ^Parser parser (.. Parser builder build))
+(def ^Parser parser (.. Parser
+                        builder
+                        (extensions [(TaskListItemsExtension/create)])
+                        build))
 
 (defmulti open-node (fn [_ctx node] (type node)))
 (defmulti close-node (fn [_ctx node] (type node)))
-(defmethod open-node :default [loc node] (prn :unknown-node node) loc)
-(defmethod close-node :default [loc _node]
-  (or (z/up loc) loc))
+
+(defmethod close-node :default [loc _node] (z/up loc))
 
 (defmethod open-node Document [loc _node] loc)
 (defmethod close-node Document [loc _node] loc)
@@ -66,32 +71,51 @@
                            :attrs {:src (.getDestination node) :title (.getTitle node)}
                            :content []}) z/down z/rightmost))
 
-(defn insert-text [loc ^Text node]
-  (z/append-child loc {:type :text :text (.getLiteral node)}))
+(defn handle-todo-list [loc ^TaskListItemMarker node]
+  (prn :todo node)
+  (-> loc
+      (z/edit assoc :type :todo-item :attrs {:checked (.isChecked node)})
+      z/up (z/edit assoc :type :todo-list)
+      z/down z/rightmost))
 
 (defn node->data [^Node node]
   (let [!loc (atom (parser/->zip {:type :doc :content []}))]
     (.accept node
              (proxy [AbstractVisitor] []
                (visit [^Node node]
-                 (prn :visit (str node) (z/node @!loc))
+                 #_ (prn :visit (str node) (z/node @!loc))
                  (assert @!loc (format "Can't add node: '%s' to an empty location" node))
                  (condp instance? node
-                   Text (swap! !loc insert-text node)
-                   (do
-                     (swap! !loc open-node node)
-                     (.visitChildren this node)
-                     (swap! !loc close-node node))))))
+                   Text (swap! !loc z/append-child {:type :text :text (.getLiteral ^Text node)})
+                   ThematicBreak (swap! !loc z/append-child {:type :ruler})
+                   SoftLineBreak (swap! !loc z/append-child {:type :softbreak})
+                   HardLineBreak (swap! !loc z/append-child {:type :softbreak})
+                   TaskListItemMarker (swap! !loc handle-todo-list node)
+                   (if (get-method open-node (class node))
+                     (do
+                       (swap! !loc open-node node)
+                       (.visitChildren this node)
+                       (swap! !loc close-node node))
+                     (prn :open-node/not-implemented node))))))
+
     (some-> @!loc z/root)))
 
 (defn parse [md]
   (node->data (.parse parser md)))
 
 (comment
-  (parse "# Ahoi
+  (do
+    (parse "# Ahoi
+
 par
+broken
+
 * a **strong** item
 * what [a nice link](/to/some 'with a title')
 * just _emphatic_ two
 
-![img](/some/src 'title')"))
+- [ ] one
+- [x] two
+---
+![img](/some/src 'title')")
+    nil))

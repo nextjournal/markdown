4a0073fc68c57ae5e60d4bc31aae799358683c1a
Michiel Borkent <michielborkent@gmail.com>
2025-06-11 23:14:36 +0200

    Make test runnable from bb CI

--- test/nextjournal/markdown/multi_threading_test.clj
+++ test/nextjournal/markdown/multi_threading_test.clj
@@ -1,11 +1,22 @@
 (ns nextjournal.markdown.multi-threading-test
-  (:require [clojure.test :as t :refer [deftest testing is]]
+  (:require [babashka.fs :as fs]
+            [clojure.java.io :as io]
+            [clojure.test :as t :refer [deftest is]]
             [nextjournal.markdown :as md]))
+
+(def notebook-file
+  ;; we do this so test can be executed from arbitrary directory (i.e. in babashka's test suite)
+  (-> (or (io/resource *file*) *file*)
+      fs/parent
+      fs/parent
+      fs/parent
+      fs/parent
+      (fs/file "notebooks" "reference.md")))
 
 (deftest multithreading
   (let [!exs (atom [])
         proc (fn []
-               (try (md/parse (slurp "notebooks/reference.md"))
+               (try (md/parse (slurp notebook-file))
                     (catch IllegalStateException e
                       (swap! !exs conj e))))
         t1 (new Thread proc)
